27735f87ad098f5d5828c1e4c2fbec212e7a1519
root <xh.shazivari@evolutivo.it>
2022-05-02 12:41:45 +0200

    i18n(ProcessStep) update italian language file

--- modules/cbProcessStep/language/it_it.lang.php
+++ modules/cbProcessStep/language/it_it.lang.php
@@ -9,13 +9,26 @@
  ************************************************************************************/
 
 $mod_strings = array(
-	'ModuleName' => 'Nome Modulo',
-	'SINGLE_ModuleName' => 'Nome Modulo',
-	'ModuleName ID' => 'ID Nome Modulo',
+	'cbProcessStep' => 'Passaggi Processo',
+	'SINGLE_cbProcessStep' => 'Passaggi Processo',
+	'cbProcessStep ID' => 'ID Passaggi Processo',
 
 	'LBL_CUSTOM_INFORMATION' => 'Informazione Personalizzata',
-	'LBL_MODULEBLOCK_INFORMATION' => 'Informazione Blocco del Modulo',
+	'LBL_CBPROCESSSTEP_INFORMATION' => 'Informazioni Passo Processo',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descrizione',
 
-	'ModuleFieldLabel' => 'Testo Etichetta Campo Modulo',
+	'processflow' => 'Flusso Processo',
+	'cbprocessstep_no' => 'No Passo processo',
+	'fromstep' => 'Da Passo',
+	'validation' => 'Validazione',
+	'tostep' => 'Al Passo',
+	'context' => 'Contesto',
+	'active' => 'Attivo',
+	'PostiveValidationTasks' => 'Compiti di Convalida Positiva',
+	'NegtiveValidationTasks' => 'Compiti di Convalida Negativa',
+	'isactivevalidation' => 'Convalida attiva',
+	'showstepvalidation' => 'Mostra convalida passo',
+	'Default Path' => 'Percorso Predefinito',
+	'Valid Default Start' => 'Inizio Predefinito Valido',
 );
 ?>
